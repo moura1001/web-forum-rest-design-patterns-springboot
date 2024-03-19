2f5c3e60f3b28c796514d10d75817b74456a26b8
moura1001 <gmouraneto01@gmail.com>
2024-03-19 10:45:19 -0300

    config: setup project

---
+++ src/main/java/com/moura1001/webforum/WebForumRestDesignPatternsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.moura1001.webforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebForumRestDesignPatternsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebForumRestDesignPatternsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moura1001/webforum/WebForumRestDesignPatternsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura1001.webforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebForumRestDesignPatternsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
